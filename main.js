38dfa8e8cf95a4e8e5a5a14f3407483761cb6e8b
felialiaga <134160663+felialiaga@users.noreply.github.com>
2023-08-16 21:16:35 -0300

    Hice la Pre Entrega número 2

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ FOR
 WHILE
 FUNCIONES
 */
-
-
+/*
+OBJETOS
+ARRAYS
+FUNCIONES ORDEN SUPERIOR
+*/
 
 
 function agregarOrganizador(nombre){
@@ -47,4 +50,55 @@ function agregarOrganizador(nombre){
     }
 }
 
-agregarOrganizador("Felipe Aliaga");
+class Eventos{
+    constructor(tipo,dia,cantidad){
+        this.tipo = tipo;
+        this.dia = dia;
+        this.cantidad = cantidad;
+    }
+}
+
+const evento = [];
+
+evento.push(new Eventos("fiesta","17/08", 250));
+evento.push(new Eventos("fiesta 18","19/09", 520));
+evento.push(new Eventos("bautismo","9/10", 150));
+evento.push(new Eventos("casamiento","31/9", 300));
+
+
+let operacion = prompt("Indique: 1) Entrar a la fiesta, 2) Programar evento");
+
+
+
+
+while (operacion != "esc") {
+    switch (operacion) {
+        case "1":
+            agregarOrganizador("Felipe Aliaga");
+            break;
+
+        case "2":
+            let tipo = prompt("Ingrese el tipo de evento:");
+            let fecha = prompt("Ingrese la fecha del evento:");
+            let cantidad = parseInt(prompt("Ingrese la cantidad de personas que asisten al evento:"));
+
+            let busqueda = evento.some((item) => item.dia === fecha);
+
+            if(busqueda){
+                alert("Lo siento! Esa fecha ya se encuentra ocupada.");
+            }else{
+                evento.push(new Eventos(tipo, fecha, cantidad));
+                alert(`Su ${tipo} fue agendada para el día ${fecha}, con un total de ${cantidad} de personas`);
+            }
+            
+
+            break;
+
+        default:
+            alert("Ingrese una opción válida.");
+            break;
+    }
+
+    operacion = prompt("Indique: 1) Entrar a la fiesta, 2) Programar fiesta");
+    
+}
